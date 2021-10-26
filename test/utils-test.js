4b5bec6b11ab5af48a77a17eeeec2c20a4df5d8b
Violet Stewart <vkazoo2@gmail.com>
2021-10-25 17:25:52 -0700

    tests utils

---
+++ test/utils-test.js
@@ -0,0 +1,39 @@
+import { generateUser, setUser } from '../utils.js';
+
+const test = QUnit.test;
+
+test('generateUser should return a userObject', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = { 
+        completed: {},
+        candy: 0,
+        hp: 3,
+        name: 'Violet',
+        costume: 'vampire',
+    };
+
+    const formData = new FormData();
+    formData.set('name', 'Violet');
+    formData.set('costume', 'vampire');
+
+    const actual = generateUser(formData);
+
+    expect.deepEqual(actual, expected);
+});
+
+test('setUser should save user to localStorage', (expect)=>{
+    localStorage.removeItem('USER');
+    const userObject = { 
+        completed: {},
+        candy: 0,
+        hp: 3,
+        name: 'Violet',
+        race: 'vampire',
+    };
+
+    setUser(userObject);
+    const actual = JSON.parse(localStorage.getItem('USER'));
+
+    expect.deepEqual(actual, userObject);
+});
