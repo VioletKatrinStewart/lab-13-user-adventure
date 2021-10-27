ae10881139c598932d20a970c7bd10d220a84d43
Violet Stewart <vkazoo2@gmail.com>
2021-10-27 14:44:16 -0700

    making map links clickable only once

--- map/map.js
+++ map/map.js
@@ -1,8 +1,26 @@
 import quests from '../data/quest-data.js';
+import { getUser } from '../utils.js';
 const mapLinks = document.getElementById('map-links');
+const user = getUser();
 for (let quest of quests){
+    if (user.completed[quest.id]){
+        displaySpan(quest);
+    } else {
+
+        displayLink(quest);
+    }
+}
+
+function displayLink(quest){
     const a = document.createElement('a');
     a.href = `../quests/?id=${quest.id}`;
     a.textContent = quest.title;
     mapLinks.appendChild(a);
+} 
+
+function displaySpan(quest){
+    const span = document.createElement('span');
+    span.textContent = quest.title;
+
+    mapLinks.appendChild(span);
 } 
