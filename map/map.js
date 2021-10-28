27b477c434d389eaf3431f6c3ec447320656836d
Violet Stewart <vkazoo2@gmail.com>
2021-10-28 09:35:35 -0700

    css

--- map/map.js
+++ map/map.js
@@ -4,10 +4,11 @@ import { getUser, loadProfile, hasCompletedAllQuests } from '../utils/utils.js';
 const mapLinks = document.getElementById('map-links');
 const user = getUser();
 
+loadProfile();
+
 if (user.hp <= 0 || hasCompletedAllQuests(user)){
     window.location.replace('../gameover');
 }
-loadProfile();
 
 for (let quest of quests){
     if (user.completed[quest.id]){
