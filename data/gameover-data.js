1e0a622637a623c4b6543bacea99074a221b0c81
Violet Stewart <vkazoo2@gmail.com>
2021-10-28 09:53:59 -0700

    data for gameover

---
+++ data/gameover-data.js
@@ -0,0 +1,14 @@
+export const lowCandy = {
+    time: 'You ran out of time and had to go back home to your parents! I hope you had a fun night out trick-or-treating, even though you did not end with too much candy.',
+    candy: 'A rats! You finished with barely any candy! Better luck next year!'
+};
+
+export const mediumCandy = {
+    time: 'You ran out of time and had to go back home to your parents! I hope you had a fun night out trick-or-treating, and it looks like you made off with a fair amount of candy! Go you!',
+    candy: 'You have a good stash. Now to decide if you eat it all in one night or ration it out for a while. Good job gettting those candy bars!'
+};
+
+export const highCandy = {
+    time: 'You ran out of time and had to go back home to your parents! I hope you had a fun night out trick-or-treating, AND IT LOOKS LIKE YOU ENDED UP WITH A TON OF CANDY BARS! How did you do that? Amazing! You are living like halloween royalty',
+    candy: 'You won big time! Look at all that candy!!! What a lucky kid! Good job!'
+}; 
